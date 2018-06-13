b2f4a6f3f0a74307ded3575c6237e9dbf6176e59
maxi <maxiejbe92@gmail.com>
2018-06-12 23:09:49 -0300

    Fixing little authorization bug

--- demo-backend/src/middleware/common/isAuthenticated.js
+++ demo-backend/src/middleware/common/isAuthenticated.js
@@ -3,7 +3,6 @@ import isNonSecurePath from './isNonSecurePath';
 
 const isAuthenticated = (req, res, next) => {
   if (isNonSecurePath(req.path)) return next();
-  
   passport.authenticate('jwt', function(err, user, info) {
     if (err || !user) { return res.status(401).json({ auth: false }); }
     return next();
