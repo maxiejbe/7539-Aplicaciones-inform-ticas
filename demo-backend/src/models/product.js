8de0fdd24b1f0b92d99bd249edb2230e3904d5cb
maxi <maxiejbe92@gmail.com>
2018-06-12 23:11:03 -0300

    Including product model and routes

---
+++ demo-backend/src/models/product.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const ProductSchema = new mongoose.Schema({
+  name: { 
+    type: String,
+    required: true
+  },
+  category: {
+    type:     mongoose.Schema.Types.ObjectId,
+    ref:      'Category',
+    required: true
+  },
+})
+
+const Product = mongoose.model('product', ProductSchema)
+export default Product
