db98de419dbeb3db6acaa96acc81f0b81542cc8d
maxi <maxiejbe92@gmail.com>
2018-06-14 10:38:56 -0300

    Including group model

---
+++ demo-backend/src/models/group.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const GroupSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  point: {
+    type: [Number],
+    index: '2d'
+  },
+  members: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+})
+
+const Group = mongoose.model('group', GroupSchema)
+export default Group
